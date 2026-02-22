7395557587df866e25fd9a6b21ac0b9da49e1585
powerjaehoonzz <powerjaehoon@gmail.com>
2026-02-22 23:42:15 +0900

    feat: add redirect endpoint for NestJS docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Redirect, Req } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,16 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(@Req() req: Request): string {
+    console.log('req :', req);
     return this.appService.getHello();
+  }
+
+  @Get('redirect/docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
   }
 }
