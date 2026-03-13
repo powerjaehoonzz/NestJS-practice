280a541bfad3bcaf8f615471c81ea5ac6dd64ced
powerjaehoonzz <powerjaehoon@gmail.com>
2026-03-13 22:45:20 +0900

    feat(auth): user repository 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+}
