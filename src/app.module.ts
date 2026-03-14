ea7b7921c8dac3278d6d5d6a734298dac29b7b68
powerjaehoonzz <powerjaehoon@gmail.com>
2026-03-14 23:48:49 +0900

    feat(auth): 사용자 생성 API 구현

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { BoardsModule } from './boards/boards.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeORMConfig } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, AuthModule],
