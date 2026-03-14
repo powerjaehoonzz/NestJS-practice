9f4fc8f9ee1a304a91bde8387d79f56fc1b133ca
powerjaehoonzz <powerjaehoon@gmail.com>
2026-03-14 23:35:24 +0900

    feat(auth): 사용자 생성 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
 @Injectable()
 export class AuthService {
@@ -9,4 +10,11 @@ export class AuthService {
     @InjectRepository(User)
     private userRepository: Repository<User>,
   ) {}
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.userRepository.create({ username, password });
+
+    await this.userRepository.save(user);
+  }
 }
