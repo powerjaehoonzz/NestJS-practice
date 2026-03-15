dfacbea050eb60aac713f862b27b23ca31f7da0c
powerjaehoonzz <powerjaehoon@gmail.com>
2026-03-15 22:45:30 +0900

    feat(auth): 회원가입 유저 데이터 유효성 체크 기능 추가

--- src/auth/dto/auth-credential.dto.ts
+++ src/auth/dto/auth-credential.dto.ts
@@ -1,4 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  @Matches(/^[a-zA-Z0-9]*$/, {
+    message: '비밀번호는 영어나 숫자만 가능합니다',
+  })
   password: string;
 }
