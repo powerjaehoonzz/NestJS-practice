747af497cc11c1d13a955f28371cd4cab0d38081
powerjaehoonzz <powerjaehoon@gmail.com>
2026-03-16 23:56:11 +0900

    feat(auth): username에 유니크한 값 주기 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
@@ -15,6 +19,14 @@ export class AuthService {
     const { username, password } = authCredentialsDto;
     const user = this.userRepository.create({ username, password });
 
-    await this.userRepository.save(user);
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
