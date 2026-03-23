f8360214576a241b5dc888ae1cb720877d9b5841
powerjaehoonzz <powerjaehoon@gmail.com>
2026-03-23 22:56:01 +0900

    feat: 유저와 게시물의 관계 형성

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,11 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Board } from 'src/boards/board.entity';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity()
 export class User extends BaseEntity {
@@ -10,4 +17,7 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  @OneToMany((type) => Board, (board) => board.user, { eager: true })
+  boards: Board[];
 }
